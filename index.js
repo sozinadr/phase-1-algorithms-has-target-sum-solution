2d05b0a21be39d962fb047ecc2c76569765a1279
sozi <sozyarnadir@gmail.com>
2022-09-13 13:46:58 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+    // Write your algorithm here
+    // Write your algorithm here
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length, i !== j; j++) {
+            if (array[i] + array[j] === target) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 /* 
@@ -16,19 +25,36 @@ function hasTargetSum(array, target) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+    // add your own custom tests in here
+    console.log("Expecting: true");
+    console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+    // add your own custom tests in here
+    console.log("Expecting: true");
+    console.log("=>", hasTargetSum([1, 4, 17, 431, 9], 440));
 
-  console.log("");
+    console.log("");
+    console.log("");
 
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+    console.log("Expecting: true");
+    console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+    console.log("Expecting: false");
+    console.log("=>", hasTargetSum([2, 2, 2, 2, 2], 2));
 
-  console.log("");
+    console.log("");
+    console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
+    console.log("Expecting: false");
+    console.log("=>", hasTargetSum([1, 2, 5], 4));
 }
+console.log("Expecting: true");
+console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
+module.exports = hasTargetSum;
+console.log("");
+console.log("Expecting: true");
+console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+console.log("");
+console.log("Expecting: false");
+console.log("=>", hasTargetSum([1, 2, 5], 4));
+}
 module.exports = hasTargetSum;
